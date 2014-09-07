d63e501e79b6f3e12558ec55138f54afda708ad9
Damien Lehericy <contact.zengeek@gmail.com>
2014-09-07 21:07:31 +0200

    [Update] Clean code, more difficulty

--- app/main.js
+++ app/main.js
@@ -1,21 +1,21 @@
-// Initialize Phaser, and creates a 400x490px game
-var game = new Phaser.Game(400, 490, Phaser.CANVAS, 'game_div');
+'use strict';
+//Define Canvas && path for assets
+var Phaser,
+    game = new Phaser.Game(400, 490, Phaser.CANVAS, 'game_div'),
     assets = {
         sprite : "assets/sprites/",
         sounds : "assets/sounds/"
     };
 
-// Creates a new 'main' state that wil contain the game
+// Create the game state
 var main_state = {
 
-    preload: function() {
-		// Background of game
+    preload: function () {
+        // Load BG
         this.game.load.image('background', assets.sprite + 'buildingOne.png');
 
-        // Load hero spritesheet (name, path, size)
+        // Load characters spritesheets (name, path, size)
         this.game.load.spritesheet('hero', assets.sprite + 'hero_spritesheet.png', 45, 45);
-
-        // Load ennemies
         this.game.load.spritesheet('ennemy', assets.sprite + 'bird_spritesheet.png', 60, 60);
 
         // Load UI
@@ -33,30 +33,32 @@ var main_state = {
         this.game.load.audio('kick', assets.sounds + 'kick.wav');
     },
 
-    create: function() {
+    create: function () {
 
         // Define global objects
-        var style  = {
-                textUI : {
-                    font: "28px Terminal", fill: "#fff"
-                }
-            };
-            animProperties = {
-                hero : {
-                    frameRate : 6
-                }
-            };
-            gameKeys = {
-                right  : game.input.keyboard.addKey(Phaser.Keyboard.D),
-                left   : game.input.keyboard.addKey(Phaser.Keyboard.Q),
-                shop   : game.input.keyboard.addKey(Phaser.Keyboard.B),
-                pause  : game.input.keyboard.addKey(Phaser.Keyboard.P),
-                restart: game.input.keyboard.addKey(Phaser.Keyboard.R)
-            };
-            cursors     = game.input.keyboard.createCursorKeys();
-            ennemy      = {};
-            lineNbr     = 3;
-            pauseStatut = false;
+        this.style  = {
+            textUI : {
+                font: "28px Terminal",
+                fill: "#fff"
+            }
+        };
+        this.animProperties = {
+            hero : {
+                frameRate : 6
+            }
+        };
+        this.gameKeys = {
+            right  : game.input.keyboard.addKey(Phaser.Keyboard.D),
+            left   : game.input.keyboard.addKey(Phaser.Keyboard.Q),
+            shop   : game.input.keyboard.addKey(Phaser.Keyboard.B),
+            pause  : game.input.keyboard.addKey(Phaser.Keyboard.P),
+            restart: game.input.keyboard.addKey(Phaser.Keyboard.R)
+        };
+
+        this.cursors     = game.input.keyboard.createCursorKeys();
+        this.ennemy      = {};
+        this.lineNbr     = 3;
+        this.pauseStatut = false;
 
         // Add background
         this.bgTile = this.game.add.tileSprite(0, 0, game.stage.bounds.width, 490, 'background');
@@ -76,15 +78,15 @@ var main_state = {
 
         this.sounds.music.background.play('', 1, true, true);
 
-    	// Create hero with animation
+        // Create hero with animation
         this.hero = this.game.add.sprite(175, 100, 'hero');
         this.hero.animations.add('stand', [0, 1]);
 
         // Create group of ennemies
         this.ennemies = game.add.group();
-        this.ennemies.createMultiple(9999,'ennemy');
+        this.ennemies.createMultiple(9999, 'ennemy');
 
-        // Create UI Object
+        // Create UI && dynamics variable of UI
         this.hero.health  = 3;
         this.score        = 0;
 
@@ -98,10 +100,10 @@ var main_state = {
             }
         };
 
-        this.UI.inGame.lifeNbrUI    = this.game.add.text(60, 20, this.hero.health, style.textUI);
-        this.UI.inGame.scoreNbrUI   = this.game.add.text(330, 20, this.score, style.textUI);
+        this.UI.inGame.lifeNbrUI    = this.game.add.text(60, 20, this.hero.health, this.style.textUI);
+        this.UI.inGame.scoreNbrUI   = this.game.add.text(330, 20, this.score, this.style.textUI);
 
-        // Allow click on buttons
+        // Add interactions with UI buttons
         this.UI.inGame.menuButtonUI.inputEnabled    = true;
         this.UI.inGame.lifeButtonUI.inputEnabled    = true;
         this.UI.inGame.restartButtonUI.inputEnabled = true;
@@ -110,57 +112,52 @@ var main_state = {
         this.UI.inGame.lifeButtonUI.input.useHandCursor    = true;
         this.UI.inGame.restartButtonUI.input.useHandCursor = true;
 
-        // Gravity
-        this.bgTile.body.velocity.y = -180;
+        // Background scroll (Hero speed move illusion)
+        this.bgTile.body.velocity.y = -200;
 
         // timer for add ennemy
         this.timer = this.game.time.events.loop(
-            1500,
+            1000,
             this.add_row_of_ennemies,
             this
         );
     },
 
-    update: function() {
-        // Cursors && Keyboard detection
-        if (cursors.left.isDown || gameKeys.left.isDown)
-        {
+    update: function () {
+        // Do right && left movement
+        if (this.cursors.left.isDown || this.gameKeys.left.isDown) {
             this.hero.animations.stop();
             this.hero.x = this.hero.x - 5;
-        }
-        else if (cursors.right.isDown || gameKeys.right.isDown)
-        {
+        } else if (this.cursors.right.isDown || this.gameKeys.right.isDown) {
             this.hero.animations.stop();
             this.hero.x = this.hero.x + 5;
-        }
-        else
-        {
-            this.hero.animations.play('stand', animProperties.hero.frameRate, true);
+        } else {
+            this.hero.animations.play('stand', this.animProperties.hero.frameRate, true);
         }
 
         // Pause statut
         this.UI.inGame.menuButtonUI.events.onInputDown.add(this.pause_game, this);
-        gameKeys.pause.onDown.add(this.pause_game, this);
+        this.gameKeys.pause.onDown.add(this.pause_game, this);
 
         // Shop
         this.UI.inGame.lifeButtonUI.events.onInputDown.add(this.buy_life, this);
-        gameKeys.shop.onDown.add(this.buy_life, this);
+        this.gameKeys.shop.onDown.add(this.buy_life, this);
 
         // Restart game
         this.UI.inGame.restartButtonUI.events.onInputDown.add(this.restart_game, this);
-        gameKeys.restart.onDown.add(this.restart_game, this);
+        this.gameKeys.restart.onDown.add(this.restart_game, this);
 
         // Check Life
         if (this.hero.health <= 0) {
             this.game_over();
         }
 
-        if (this.hero.alive == false) {
+        if (this.hero.alive === false) {
             this.restart_game();
         }
 
         // Drain life, if without world
-        if (this.hero.inWorld == false) {
+        if (this.hero.inWorld === false) {
             this.hero.health = this.hero.health - 1;
         }
 
@@ -182,33 +179,34 @@ var main_state = {
 
     add_one_ennemy: function (x, y) {
         // first dead ennemy of our group
-        ennemy = this.ennemies.getFirstDead();
+        this.ennemy = this.ennemies.getFirstDead();
 
         // new position of ennemy
-        ennemy.reset(x, y);
+        this.ennemy.reset(x, y);
 
         // Animate ennemy
-        ennemy.animations.add('stand', [0, 1]);
-        ennemy.animations.play('stand', 10, true);
+        this.ennemy.animations.add('stand', [0, 1]);
+        this.ennemy.animations.play('stand', 10, true);
 
         // Move to top
-        ennemy.body.velocity.y = -200;
+        this.ennemy.body.velocity.y = -260;
 
         // Kill ennemy when he disapear
-        ennemy.outOfBoundsKill = true;
+        this.ennemy.outOfBoundsKill = true;
     },
 
+    // Add row of ennemies (nbrEnnemy, margin, size of line)
     add_row_of_ennemies: function () {
         var hole = Math.floor(Math.random() * 9) + 1,
-            birdNbr = Math.floor(Math.random() * 8),
+            birdNbr = Math.floor(Math.random() * 6) + 1,
             i;
 
         for (i = 0; i < birdNbr; i += 1) {
-            if (i != hole && i != hole + 3) {
+            if (i !== hole && i !== hole + 2) {
                 this.add_one_ennemy(i * 70 + -15, 490);
             }
         }
-            
+
         this.score += 1;
     },
 
@@ -216,14 +214,14 @@ var main_state = {
         // Define health after each hit
         this.hero.health = this.hero.health - 1;
 
-        if(this.hero.health < 0) {
+        if (this.hero.health < 0) {
             this.hero.health = 0;
         }
 
         if (!this.sounds.effects.kick.isPlaying || !this.sounds.effects.bird.isPlaying) {
             if (this.sounds.effects.bird.totalDuration > 0) {
-               this.sounds.effects.bird.stop();
-               this.sounds.effects.bird.play();
+                this.sounds.effects.bird.stop();
+                this.sounds.effects.bird.play();
             } else {
                 this.sounds.effects.kick.play();
                 this.sounds.effects.bird.play();
@@ -232,10 +230,10 @@ var main_state = {
     },
 
     buy_life: function () {
-        if(this.score >= 3) {
-            this.score = this.score - 3;
+        if (this.score >= 5) {
+            this.score = this.score - 5;
             this.hero.health = this.hero.health + 1;
-        }        
+        }
     },
 
     game_over : function () {
@@ -249,7 +247,7 @@ var main_state = {
         // Prevent new ennemies from appearing
         this.game.time.events.remove(this.timer);
 
-        if (this.hero.inWorld == false) {
+        if (this.hero.inWorld === false) {
             this.hero.alive = false;
         }
     },
